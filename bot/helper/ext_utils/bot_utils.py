bef53be6753e9268f8b3e070849fd6cebede1908
Umsh29359dj <105089099+Umsh29359dj@users.noreply.github.com>
2022-09-23 20:26:49 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -108,7 +108,7 @@ def get_progress_bar_string(status):
     cFull = p // 8
     p_str = '.' * cFull
     p_str += '𝅶' * (12 - cFull)
-    p_str = f"[{p_str}]"
+    p_str = f"𝅶{p_str}𝅶"
     return p_str
 
 def get_readable_message():
