10ecd2e86a2815b9287d500f1a4d424fab717c7d
Umsh29359dj <105089099+Umsh29359dj@users.noreply.github.com>
2022-09-23 20:25:36 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -20,8 +20,8 @@ PAGE_NO = 1
 
 
 class MirrorStatus:
-    STATUS_UPLOADING = "📤"
-    STATUS_DOWNLOADING = "📥"
+    STATUS_UPLOADING = " 📤"
+    STATUS_DOWNLOADING = " 📥"
     STATUS_CLONING = "♻️"
     STATUS_WAITING = "Queue"
     STATUS_PAUSED = "⏸️"
@@ -168,8 +168,8 @@ def get_readable_message():
                     up_speed += float(spd.split('K')[0]) * 1024
                 elif 'M' in spd:
                     up_speed += float(spd.split('M')[0]) * 1048576
-        bmsg = f"<b>🖥 </b> {cpu_percent()}% | <b>💎 </b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}"
-        bmsg += f"\n<b>🧰 </b> {virtual_memory().percent}% <b>🔮 </b> {get_readable_time(time() - botStartTime)}"
+        bmsg = f"<b>🖥 </b> {cpu_percent()}% | <b></b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}"
+        bmsg += f"\n<b>🧰 </b> {virtual_memory().percent}% <b></b> {get_readable_time(time() - botStartTime)}"
         bmsg += f"\n<b>🔻</b> {get_readable_file_size(dl_speed)}/s  <b>🔺 </b> {get_readable_file_size(up_speed)}/s"
         if STATUS_LIMIT is not None and tasks > STATUS_LIMIT:
             msg += f"<b>Page:</b> {PAGE_NO}/{pages} | <b>Tasks:</b> {tasks}\n"
