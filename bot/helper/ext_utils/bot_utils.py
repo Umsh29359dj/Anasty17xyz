6442561baf12eb4ed4705d5a948dec09b00f26d5
Umsh29359dj <105089099+Umsh29359dj@users.noreply.github.com>
2022-09-23 19:40:58 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -107,7 +107,7 @@ def get_progress_bar_string(status):
     p = min(max(p, 0), 100)
     cFull = p // 8
     p_str = '❤' ∆'¶' * cFull
-    p_str += '☠️'💯'😂' * (12 - cFull)
+    p_str += '' * (12 - cFull)
     p_str = f"[{p_str}]"
     return p_str
 
