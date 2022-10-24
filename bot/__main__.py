d8bf51e24d125fb61770a072d0f418fb22316d7c
Umsh29359dj <105089099+Umsh29359dj@users.noreply.github.com>
2022-10-24 18:53:39 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -26,16 +26,16 @@ def stats(update, context):
     total, used, free, disk = disk_usage('/')
     swap = swap_memory()
     memory = virtual_memory()
-    stats = f'<b>Commit Date:</b> {last_commit}\n\n'\
-            f'<b>Bot Uptime:</b> {get_readable_time(time() - botStartTime)}\n'\
+    stats = f'<b>🔬 </b> {last_commit}\n\n'\
+            f'<b>🧭 </b> {get_readable_time(time() - botStartTime)}\n'\
             f'<b>OS Uptime:</b> {get_readable_time(time() - boot_time())}\n\n'\
             f'<b>Total Disk Space:</b> {get_readable_file_size(total)}\n'\
             f'<b>Used:</b> {get_readable_file_size(used)} | <b>Free:</b> {get_readable_file_size(free)}\n\n'\
-            f'<b>UL :</b> {get_readable_file_size(net_io_counters().bytes_sent)}\n'\
-            f'<b>DL :</b> {get_readable_file_size(net_io_counters().bytes_recv)}\n\n'\
+            f'<b>🔺 </b> {get_readable_file_size(net_io_counters().bytes_sent)}\n'\
+            f'<b>🔻 </b> {get_readable_file_size(net_io_counters().bytes_recv)}\n\n'\
             f'<b>🖥 </b> {cpu_percent(interval=0.5)}%\n'\
-            f'<b>RAM:</b> {memory.percent}%\n'\
-            f'<b>DISK:</b> {disk}%\n\n'\
+            f'<b>🧰 </b> {memory.percent}%\n'\
+            f'<b>💌 </b> {disk}%\n\n'\
             f'<b>Physical Cores:</b> {cpu_count(logical=False)}\n'\
             f'<b>Total Cores:</b> {cpu_count(logical=True)}\n\n'\
             f'<b>SWAP:</b> {get_readable_file_size(swap.total)} | <b>Used:</b> {swap.percent}%\n'\
