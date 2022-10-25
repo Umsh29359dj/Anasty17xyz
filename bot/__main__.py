dbb5fd45113fc74a7a372fa53994a790521c6ae6
Umsh29359dj <105089099+Umsh29359dj@users.noreply.github.com>
2022-10-25 09:40:36 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -41,7 +41,7 @@ def stats(update, context):
             f'<b>SWAP:</b> {get_readable_file_size(swap.total)} | <b>Used:</b> {swap.percent}%\n'\
             f'<b>Memory Total:</b> {get_readable_file_size(memory.total)}\n'\
             f'<b>Memory Free:</b> {get_readable_file_size(memory.available)}\n'\
-            f'<b>Memory Used:</b> {get_readable_file_size(memory.used)}\n'
+            f'<b>Memory Used:</b> {get_readable_file_size(memory.used)}\n\n<b>⌈➳ π- Bot Update Info..! </b>'"<a href='https://telegra.ph/file/b02788a8c2c7ca546d369.jpg'>😎</a>"
     sendMessage(stats, context.bot, update.message)
 
 
