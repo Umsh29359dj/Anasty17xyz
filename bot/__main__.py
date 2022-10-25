491886a14c56d1cde15a130388c54a03d43d5cfd
Umsh29359dj <105089099+Umsh29359dj@users.noreply.github.com>
2022-10-25 09:49:55 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -180,7 +180,7 @@ def main():
     elif not notifier_dict and AUTHORIZED_CHATS:
         for id_ in AUTHORIZED_CHATS:
             try:
-                bot.sendMessage(id_, "Bot Restarted!", 'HTML')
+                bot.sendMessage(id_, "online..! ❤", 'HTML')
             except Exception as e:
                 LOGGER.error(e)
 
