e1d6f0e18e52ad03bdae3f5eaa70d7adc554cacf
Umsh29359dj <105089099+Umsh29359dj@users.noreply.github.com>
2022-10-25 09:48:44 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -26,29 +26,29 @@ def stats(update, context):
     total, used, free, disk = disk_usage('/')
     swap = swap_memory()
     memory = virtual_memory()
-    stats = f'<b>Commit Date:</b> {last_commit}\n\n'\
-            f'<b>Bot Uptime:</b> {get_readable_time(time() - botStartTime)}\n'\
-            f'<b>OS Uptime:</b> {get_readable_time(time() - boot_time())}\n\n'\
-            f'<b>Total Disk Space:</b> {get_readable_file_size(total)}\n'\
+    stats = f'<b>🔰 </b> {last_commit}\n\n'\
+            f'<b>Uptime:</b> {get_readable_time(time() - botStartTime)}\n'\
+            f'<b>🧭 </b> {get_readable_time(time() - boot_time())}\n\n'\
+            f'<b>🛅 </b> {get_readable_file_size(total)}\n'\
             f'<b>Used:</b> {get_readable_file_size(used)} | <b>Free:</b> {get_readable_file_size(free)}\n\n'\
-            f'<b>Upload:</b> {get_readable_file_size(net_io_counters().bytes_sent)}\n'\
-            f'<b>Download:</b> {get_readable_file_size(net_io_counters().bytes_recv)}\n\n'\
-            f'<b>CPU:</b> {cpu_percent(interval=0.5)}%\n'\
-            f'<b>RAM:</b> {memory.percent}%\n'\
-            f'<b>DISK:</b> {disk}%\n\n'\
+            f'<b>🔺 </b> {get_readable_file_size(net_io_counters().bytes_sent)}\n'\
+            f'<b>🔻 </b> {get_readable_file_size(net_io_counters().bytes_recv)}\n\n'\
+            f'<b>🖥 </b> {cpu_percent(interval=0.5)}%\n'\
+            f'<b>🛄 </b> {memory.percent}%\n'\
+            f'<b>📇 </b> {disk}%\n\n'\
             f'<b>Physical Cores:</b> {cpu_count(logical=False)}\n'\
             f'<b>Total Cores:</b> {cpu_count(logical=True)}\n\n'\
-            f'<b>SWAP:</b> {get_readable_file_size(swap.total)} | <b>Used:</b> {swap.percent}%\n'\
+            f'<b>💌 </b> {get_readable_file_size(swap.total)} | <b>Used:</b> {swap.percent}%\n'\
             f'<b>Memory Total:</b> {get_readable_file_size(memory.total)}\n'\
             f'<b>Memory Free:</b> {get_readable_file_size(memory.available)}\n'\
-            f'<b>Memory Used:</b> {get_readable_file_size(memory.used)}\n\n<b>⌈➳ π- Bot Update Info..! </b>'"<a href='https://telegra.ph/file/b02788a8c2c7ca546d369.jpg'>😎</a>"
+            f'<b>Memory Used:</b> {get_readable_file_size(memory.used)}\n\n<b>⌈➳Bot Update Info..! </b>'"<a href='https://telegra.ph/file/b02788a8c2c7ca546d369.jpg'>😎</a>"
     sendMessage(stats, context.bot, update.message)
 
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Owner", "https://www.github.com/anasty17")
+    buttons.buildbutton("❤", "https://t.me/at_rai")
+    buttons.buildbutton("Owner", "https://t.me/at_rai")
     reply_markup = buttons.build_menu(2)
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
