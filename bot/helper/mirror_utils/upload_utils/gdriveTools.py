25585fa9e4054ec99761142d33cc6e84d8c9a11b
Umsh29359dj <105089099+Umsh29359dj@users.noreply.github.com>
2022-10-29 05:39:17 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -264,7 +264,7 @@ class GoogleDriveHelper:
     def __create_directory(self, directory_name, parent_id):
         file_metadata = {
             "name": directory_name,
-            "description": "π-TEK Exclusive ❤",
+            "description": "❤",
             "mimeType": self.__G_DRIVE_DIR_MIME_TYPE
         }
         if parent_id is not None:
@@ -282,7 +282,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'π-TEK Exclusive ❤',
+            'description': '❤',
             'mimeType': mime_type,
         }
         if parent_id is not None:
